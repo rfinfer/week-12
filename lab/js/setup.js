f716ee6e45b953e4250de54093a25a7fb2cd4fd5
rfinfer <rachelfinfer@gmail.com>
2017-05-02 14:14:32 -0400

    May 2 final check in

--- lab/js/setup.js
+++ lab/js/setup.js
@@ -73,10 +73,12 @@ from the geometry you've drawn.
 /** Notice the use of L.Map here. This is an extension of an organizational strategy we've already discussed. */
 var app = {
   apikey: "8aa0bfd58931875bd27eb556a286210717e4ea67",
-  map: L.map('map', { drawcontrol:true, center: [40.75583970971843, -73.795166015625], zoom: 3 }),
+  map: L.map('map', { drawcontrol:true, center: [39.952052, -75.164004], zoom: 13 }),
   geojsonClient: new cartodb.SQL({ user: 'rfinfer', format: 'geojson' }),
+  jsonClient: new cartodb.SQL({ user: 'rfinfer', format: 'json' }),
   drawnItems: new L.FeatureGroup()
 };
+
 
 L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -98,7 +100,7 @@ var allData;
 // The initial query by which we map the geojson representation of a table
 app.geojsonClient.execute("SELECT * FROM crash2011_2014v2") // 'LIMIT' should be added to the end of this line
   .done(function(data) {
-    allData = data;
+    //allData = data;
     // L.geoJson(data, {
     //   onEachFeature: function(feature, layer) {
     //     layer.on('click', function() { fillForm(feature.properties); });
@@ -106,39 +108,76 @@ app.geojsonClient.execute("SELECT * FROM crash2011_2014v2") // 'LIMIT' should be
     // }).addTo(app.map);
 
 
-    var heatLayerArray = data.features.map(function(feature){
-        return feature.geometry.coordinates.reverse()
-    	});
-    //Leaflet heat setup
-    var heat = L.heatLayer(
-    	heatLayerArray, {radius: 25}).addTo(app.map);
-  })
-  .error(function(errors) {
+  //   var heatLayerArray = data.features.map(function(feature){
+  //       return feature.geometry.coordinates.reverse()
+  //   	});
+  //   //Leaflet heat setup
+  //   var heat = L.heatLayer(
+  //   	heatLayerArray, {radius: 25}).addTo(app.map);
+  // })
+  // .error(function(errors) {
+
   });
 
 
+//chartist setup
+//this is the sql code to create the chart
+//SELECT crash_year, count(*), sum(fatal_count) as fatal_count_sum, sum(maj_inj_count) as maj_inj_count_sum FROM crash2011_2014v2 GROUP BY crash_year ORDER BY crash_year
+
+
+app.jsonClient.execute("SELECT crash_year, count(*), sum(fatal_count) as fatal_count_sum, sum(maj_inj_count) as maj_inj_count_sum FROM crash2011_2014v2 GROUP BY crash_year ORDER BY crash_year")
+  .done(function(data) {
+    allData = data;
+
+    console.log(data)
+    var fatalArray = allData.rows.map(function(feature){
+      return feature.fatal_count_sum
+    });
+    console.log(fatalArray);
+
+    var seriousArray = allData.rows.map(function(feature){
+      return feature.maj_inj_count_sum
+    });
+    console.log(seriousArray);
+
+    var totalArray = allData.rows.map(function(feature){
+      return feature.count
+    });
+    console.log(totalArray);
 
 
 
 
-// Leaflet draw setup
-app.map.addLayer(app.drawnItems);
+    new Chartist.Bar('.ct-chart', {
+      labels: ['2011', '2012', '2013', '2014'],
+      series: [
+        //total = allData.rows.count
+        //totalArray,
+        //serious
+        seriousArray,
+        //fatal
+        fatalArray
+      ]
+    }, {
+      seriesBarDistance: 5,
+      axisX: {
+        offset: 60
+      },
+      axisY: {
+        offset: 80,
+        labelInterpolationFnc: function(value) {
+          return value + ''
+        },
+        scaleMinSpace: 15
+      }
+    });
+})
 
-// Initialise the draw control and pass it the FeatureGroup of editable layers
-app.map.addControl(
-  new L.Control.Draw({
-    edit: {
-      featureGroup: app.drawnItems
-    },
-    draw: {
-      rectangle: false,
-      polyline: false,
-      polygon: true,
-      marker: false,
-      circle: false
-    }
-  })
-);
+  .error(function(errors) {
+    console.log("barchart carto errors:", errors);
+  });
+
+//allData.rows[0].count
 
 // Automatically fill the form on the left from geojson response
 var fillForm = function(properties) {
@@ -146,11 +185,73 @@ var fillForm = function(properties) {
   $('#person_cou').val(properties.person_cou);
 };
 
-// Handling the creation of Leaflet.Draw layers
-// Note the use of drawnLayerID - this is the way you should approach remembering and removing layers
-var drawnLayerID;
-app.map.on('draw:created', function (e) {
-  var type = e.layerType;
-  var layer = e.layer;
-  console.log('draw created:', e);
+
+//need to use set.SQL to filter between tabs: SELECT * FROM crash2011_2014v2 WHERE ped_count>0
+//USING SET.SQL TO FILTER
+console.log("appmap",app.map);
+var pointLayer = cartodb.createLayer(app.map, {
+     user_name: 'rfinfer',
+     type: 'cartodb',
+     sublayers: [
+       {
+         cartocss: "#layer { marker-fill: pink; }", //need to see if this should be filled in
+         sql: "SELECT * FROM crash2011_2014v2", //what you want to show up originally
+       }
+     ]
+
+ }).addTo(app.map)
+ .on('done', function(layer) {
+   // Set interactivity
+   layer.setInteraction(true);
+   $('#Ped').click(function(){ //need to switch from tabs to buttons??
+     console.log("ped button clicked");
+     layer.getSubLayer(0).setSQL("SELECT * FROM crash2011_2014v2 WHERE ped_count>0");
+      layer.getSubLayer(0).setCartoCSS("#layer { marker-fill: red;}")
+   });
+   $('#Bike').click(function(){
+     console.log("bike button clicked");
+     layer.getSubLayer(0).setSQL("SELECT * FROM crash2011_2014v2 WHERE bicycle_count>0");
+     layer.getSubLayer(0).setCartoCSS("#layer { marker-fill: blue;}")
+
+   }); //what you want to show up on the button
+   $('#All').click(function(){
+     console.log("all button clicked");
+     layer.getSubLayer(0).setSQL("SELECT * FROM crash2011_2014v2");
+        layer.getSubLayer(0).setCartoCSS("#layer { marker-fill: pink;}")
+   })
+ }).on('error', function(error) {
+    console.log("some error occurred",error);
 });
+
+//   }).addTo(app.map).on('shown.bs.tab', function (e) {
+//    layer.setInteraction(true);
+//    $('a[data-toggle="tab"]').on(function(){
+//      var target = $(e.target).attr("href") // activated tab
+//      console.log("target");
+//    })
+//  }).on('error', function(error) {
+//       console.log("some error occurred",error);
+//   });
+
+
+
+
+
+
+ //$('#style1').click(function() {
+  //     layer.getSubLayer(0).setCartoCSS('#pacd_2011 { line-width: 5; line-color: #000; }');
+  //   });
+
+  //  layer.on('zoomend',function(e) {
+  //    console.log(data);
+  //  });
+
+
+
+//USING ZOOMEND TO CHANGE BACK TO POINTS
+// map.on('zoomend',(function(e) {
+// map.removeLayer(heatLayer);
+
+//});
+//zoom_based_layer_change();
+//function zoom_based_layerchange() {

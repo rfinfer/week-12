69ce5f3324d71c30c0a015544df182e0a1e2ec86
rfinfer <rachelfinfer@gmail.com>
2017-05-08 17:07:30 -0400

    speed limits updated

--- lab/js/setup.js
+++ lab/js/setup.js
@@ -109,11 +109,11 @@ var pointLayer = cartodb.createLayer(app.map, {
        {
          //cartocss: '#layer { line-width: 1.5; line-color: red;}',
          //cartocss: '#layer {line-width: 1; line-opacity: 0.5;[speedlimit = "25"]{line-color: #6c2167;}}',
-         cartocss: '#layer {line-width: 2.5; [speedlimit = "25"]{line-color: #fff7f3;}[speedlimit = "30"]{line-color: #fde0dd;}[speedlimit = "35"]{line-color: #fdbb84;}[speedlimit = "40"]{line-color: #fa9fb5;}[speedlimit = "45"]{line-color: #f768a1;}[speedlimit = "CLASS 1"]{line-color: #dd3497;}[speedlimit = "CLASS 2"]{line-color: #ae017e;}[speedlimit = "CLASS 3"]{line-color: #7a0177;}[speedlimit = "CLASS 4"]{line-color: #49006a;}[speedlimit = "CLASS 12"]{line-color: #3f007d;}}',
+         cartocss: '#layer {line-width: 2.5; [speedlimit = "25"]{line-color: #fcc5c0;}[speedlimit = "30"]{line-color: #f768a1;}[speedlimit = "35"]{line-color: #c51b8a;}[speedlimit = "40"]{line-color: #7a0177;}[speedlimit = "45"]{line-color: #7a0177;}[speedlimit = "CLASS 1"]{line-color: #7a0177;}[speedlimit = "CLASS 2"]{line-color: #c51b8a;}[speedlimit = "CLASS 3"]{line-color: #fcc5c0;}[speedlimit = "CLASS 4"]{line-color: #feebe2;}[speedlimit = "CLASS 12"]{line-color: white;}}',
          sql: "SELECT * FROM streets", //what you want to show up originally
        },
        {
-         cartocss: "#layer { marker-fill: #5642f4; }", //need to see if this should be filled in
+         cartocss: "#layer { marker-fill: #016c59; }", //need to see if this should be filled in
          sql: "SELECT * FROM crash2011_2014v2", //what you want to show up originally
          interactivity: ['person_count'],
        }
@@ -140,7 +140,7 @@ var pointLayer = cartodb.createLayer(app.map, {
    $('#Ped').click(function(){
      console.log("ped button clicked");
      pointSubLayer.setSQL("SELECT * FROM crash2011_2014v2 WHERE ped_count>0");
-     pointSubLayer.setCartoCSS("#layer { marker-fill: #5cc490;}")
+     pointSubLayer.setCartoCSS("#layer { marker-fill: #67a9cf;}")
       //CHART FOR PED CRASHES
       app.jsonClient.execute("SELECT crash_year, count(*), sum(fatal_count) as fatal_count_sum, sum(maj_inj_count) as maj_inj_count_sum FROM crash2011_2014v2 WHERE ped_count>0 GROUP BY crash_year ORDER BY crash_year")
         .done(function(data) {
@@ -202,7 +202,7 @@ var pointLayer = cartodb.createLayer(app.map, {
    $('#Bike').click(function(){
      console.log("bike button clicked");
      pointSubLayer.setSQL("SELECT * FROM crash2011_2014v2 WHERE bicycle_count>0");
-     pointSubLayer.setCartoCSS("#layer { marker-fill: #45b7cc;}")
+     pointSubLayer.setCartoCSS("#layer { marker-fill: #1c9099; }")
      app.jsonClient.execute("SELECT crash_year, count(*), sum(fatal_count) as fatal_count_sum, sum(maj_inj_count) as maj_inj_count_sum FROM crash2011_2014v2 WHERE bicycle_count>0 GROUP BY crash_year ORDER BY crash_year")
        .done(function(data) {
          allData = data;
@@ -258,7 +258,7 @@ var pointLayer = cartodb.createLayer(app.map, {
    $('#All').click(function(){
      console.log("all button clicked");
      pointSubLayer.setSQL("SELECT * FROM crash2011_2014v2");
-     pointSubLayer.setCartoCSS("#layer { marker-fill: #5642f4;}")
+     pointSubLayer.setCartoCSS("#layer { marker-fill: #016c59;}")
      app.jsonClient.execute("SELECT crash_year, count(*), sum(fatal_count) as fatal_count_sum, sum(maj_inj_count) as maj_inj_count_sum FROM crash2011_2014v2 GROUP BY crash_year ORDER BY crash_year")
           .done(function(data) {
             allData = data;
